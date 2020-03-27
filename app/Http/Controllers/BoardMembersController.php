a9d6b53b608df4f2ddea793401df3bf48cf60792
Sajed Ahmed Khan <sajedaiub@gmail.com>
2020-03-27 22:05:55 +0600

    Revert "Revert "add auth to constructor for all contriller""
    This reverts commit 392ab06677400724ccfbf33ddb5b4e1f92df16f5.

--- app/Http/Controllers/BoardMembersController.php
+++ app/Http/Controllers/BoardMembersController.php
@@ -8,6 +8,12 @@ use DB;
 
 class BoardMembersController extends Controller
 {
+	
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+	
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/MemberPersonalInfosController.php
+++ app/Http/Controllers/MemberPersonalInfosController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class MemberPersonalInfosController extends Controller
 {
+	
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+	
     /**
      * Display a listing of the resource.
      *
