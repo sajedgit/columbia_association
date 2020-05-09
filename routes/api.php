3f1af738cfb6cbb9184ee7251102596da233a700
Sajed Ahmed <sajed.ahmed@synesisit.com.bd>
2020-05-10 05:04:06 +0600

    Update Message and Sponsor API

--- routes/api.php
+++ routes/api.php
@@ -33,7 +33,6 @@ Route::middleware('auth:api')->group(function () {
     Route::resource('sponsors', 'api\SponsorsController');
 });
 
-Route::resource('messages', 'api\MessagesController');
 Route::get('/clear-cache', function() {
     $exitCode = Artisan::call('config:cache');
     return 'DONE'; //Return anything
