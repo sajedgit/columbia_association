48c9a6d2fc79dbca73da2c47252e93641c7e20d8
Sajed Ahmed <sajed.ahmed@synesisit.com.bd>
2021-01-13 13:27:52 +0600

    update profile update

--- app/Http/Controllers/api/MembershipsController.php
+++ app/Http/Controllers/api/MembershipsController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers\api;
+use App\Models\MemberJobInfo;
+use App\Models\MemberPersonalInfo;
 use App\Models\Membership;
+use App\Models\MembershipPayment;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Validator;
+use Illuminate\Support\Facades\DB;
 
 class MembershipsController extends Controller
 {
@@ -87,18 +91,140 @@ class MembershipsController extends Controller
 			 }
 
 
-			$result->update($request->all());
+//			$result->update($request->all());
+//
+//			if ($result)
+//				return response()->json([
+//					'success' => true,
+//					'data' => $result
+//				]);
+//			else
+//				return response()->json([
+//					'success' => false,
+//					'message' => 'Item could not be updated'
+//				]);
 
-			if ($result)
-				return response()->json([
-					'success' => true,
-					'data' => $result
-				]);
-			else
-				return response()->json([
-					'success' => false,
-					'message' => 'Item could not be updated'
-				]);
+            $validator = Validator::make($request->all(), [
+                'name' => 'min:3',
+                'password' => 'min:6',
+                'member_birth_date' => 'date',
+                'member_appointment_date' => 'date',
+                'member_promoted_date' => 'date',
+
+            ]);
+
+            if ($validator->fails()) {
+
+                return response()->json(['success'=>false,'error'=>$validator->errors()], 401);
+            }
+
+            DB::beginTransaction();
+
+            try {
+
+                $login_data = array(
+                    'name'            =>   $request->name,
+                  //  'username'        =>   $request->username,
+                    'password'        =>   bcrypt($request->password),
+                 //   'email'           =>   $request->email,
+                    'user_type_id'    =>   2,
+                    'active'          =>   1,
+                    'updated_at'      =>   date("Y-m-d")
+                );
+
+               // Membership::whereId($id)->update($request->all());
+                $result->update($request->all());
+
+                $personal_data = array(
+                    'ref_member_personal_info_membership_id' => $id,
+                    'member_first_name' =>   $request->member_first_name,
+                    'member_last_name'  =>   $request->member_last_name,
+                    'member_birth_date' =>   $request->member_birth_date,
+                    'member_email_address' =>  $request->member_email_address,
+                    'member_gender'     =>   $request->member_gender,
+                    'member_address'    =>   $request->member_address,
+                    'member_zip_code'   =>   $request->member_zip_code,
+                    'member_tax_reg_no' =>   $request->member_tax_reg_no,
+                    'member_personal_info_creating_datetime'        =>   date("Y-m-d"),
+                    'member_personal_info_editing_datetime'        =>   date("Y-m-d")
+                );
+
+                $job_data = array(
+                    'ref_member_job_info_membership_id' => $id,
+                    'member_command_code'      =>  $request->member_command_code,
+                    'member_command_name'      =>  $request->member_command_name,
+                    'member_rank'              =>   $request->member_rank,
+                    'member_shield'            =>   $request->member_shield,
+                    'member_appointment_date'  =>  $request->member_appointment_date,
+                    'member_promoted_date'     =>  $request->member_promoted_date,
+                    'member_boro'              =>  $request->member_boro,
+                    'member_benificiary'       => $request->member_benificiary,
+                    'member_reference_no'      => $request->member_reference_no,
+                    'member_retired'           => $request->member_retired,
+                    'member_job_info_creating_datetime' =>   date("Y-m-d"),
+                    'member_job_info_editing_datetime'  =>   date("Y-m-d")
+                );
+
+                $payment_data = array(
+                    'ref_membership_id' => $id,
+                    'membership_payment_ess' =>   $request->membership_payment_ess,
+                    'membership_payment_by' =>   $request->membership_payment_by,
+                    'membership_payment_details' =>   $request->membership_payment_details,
+                    'membership_payment_amount' =>   $request->membership_payment_amount,
+                    'membership_next_renewal_date' =>   $request->membership_next_renewal_date,
+                    'membership_payment_datetime' => date("Y-m-d"),
+                    'membership_payment_creating_datetime'  =>   date("Y-m-d"),
+                    'membership_payment_editing_datetime'   =>   date("Y-m-d")
+                );
+
+
+               // $result = MemberPersonalInfo::find($id);
+
+              //  MemberPersonalInfo::where('ref_member_personal_info_membership_id', $id)->update($request->all());
+              //  MemberJobInfo::where('ref_member_job_info_membership_id', $id)->update($request->all());
+              //  MembershipPayment::where('ref_membership_id', $id)->update($request->all());
+
+                $user_personal_infos = DB::select(DB::raw(" SELECT * from member_personal_infos where  ref_member_personal_info_membership_id=$id  "));
+                if(count($user_personal_infos))
+                    MemberPersonalInfo::where("ref_member_personal_info_membership_id",$id)->update($personal_data);
+                else
+                    MemberPersonalInfo::create($personal_data);
+
+
+                $user_job_infos = DB::select(DB::raw(" SELECT * from member_job_infos where  ref_member_job_info_membership_id=$id  "));
+                if(count($user_job_infos))
+                    MemberJobInfo::where("ref_member_job_info_membership_id",$id)->update($job_data);
+                else
+                    MemberJobInfo::create($job_data);
+
+
+
+                $membership_payments_infos = DB::select(DB::raw(" SELECT * from membership_payments where  ref_membership_id=$id  "));
+                if(count($membership_payments_infos))
+                    MembershipPayment::where("ref_membership_id",$id)->update($payment_data);
+                else
+                    MembershipPayment::create($payment_data);
+
+
+
+
+                DB::commit();
+
+
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Item with id ' . $id . ' update successfully',
+                    'data' => $this->get_member_details_by_id($id)
+
+                ]);
+
+            } catch (\Exception $e) {
+                DB::rollback();
+                return response()->json(['success'=>false,'error'=>$e], 401);
+            }
+
+
+
 
         }
 
@@ -126,6 +252,24 @@ class MembershipsController extends Controller
 				]);
 			}
         }
+
+
+
+
+
+        public function get_member_details_by_id($id)
+        {
+            $result = Membership::with('member_personal_infos','member_job_infos','membership_payments','member_devices')
+                ->where('id',$id)
+                ->orderBy('id', 'desc')
+                ->get();
+
+
+            return $result;
+        }
+
+
+
 }
 
 
--- routes/api.php
+++ routes/api.php
@@ -32,10 +32,11 @@ Route::middleware('auth:api')->group(function () {
     Route::resource('memories','api\MemorisController');
     Route::resource('sponsors', 'api\SponsorsController');
     Route::resource('messages', 'api\MessagesController');
-    // Route::post('vote', 'api\VoteDetailsController@index'); //previous vote
+    Route::post('vote', 'api\VoteDetailsController@index'); //previous vote
     Route::post('vote/{id}', 'api\VoteDetailsController@check_user_vote'); //new vote (added by raju)
     Route::post('insert_vote', 'api\VoteDetailsController@insert_vote');
     Route::post('shop', 'api\ProductsController@index');
+    Route::post('buy_product/{id}', 'api\ProductsController@buy_product');
     Route::post('slider_photo', 'api\SlidersController@index');
 });
 
