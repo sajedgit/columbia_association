9ae4963db00addbb89883784d3c2a9ded9f7360c
Sajed Ahmed <sajed.ahmed@synesisit.com.bd>
2021-03-02 11:55:06 +0600

    set up admin email

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -11,11 +12,18 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    protected  function getStatusItem()
+    protected function getStatusItem()
     {
-        $status_items=array('Select'=>'Select','1'=>'Active','0'=>'Inactive');
+        $status_items = array('Select' => 'Select', '1' => 'Active', '0' => 'Inactive');
         return $status_items;
 
+    }
+
+    protected function adminEmail()
+    {
+
+        $data = Setting::findOrFail(1);
+        return $data->admin_email;
     }
 
 }
