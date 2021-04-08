17b5bef54e72204b0d410e215df9a2c412c9bf81
sajed <sajedaiub@gmail.com>
2021-04-08 18:08:46 +0600

    update login api

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -65,114 +65,182 @@ class PassportController extends Controller
             return response()->json(['success' => false, 'error' => $validator->errors()], 401);
         }
 
-        $users_ess_type="Cash";
-        $users_ess_id=0;
-       if( $request->membership_payment_ess==1)
-       {
-           $form_data = array(
-               'type'       =>  $users_ess_type,
-               'name'        =>   $request->name,
-               'email'        =>   $request->email,
-               'created_at' => date('Y-m-d H:i:s'),
-               'active'        =>   1,
-
-           );
-
-           $create_ess_member=EssMember::create($form_data);
-           $users_ess_id=$create_ess_member->id;
-
-       }
 
 
+        if( $request->membership_payment_ess===0)
+        {
 
-        DB::beginTransaction();
+            DB::beginTransaction();
 
-        try {
+            try {
 
-//            $login_data = array(
-//                'name' => $request->name,
-//                'username' => $request->username,
-//                'password' => bcrypt($request->password),
-//                'email' => $request->email,
-//                'user_type_id' => 2,
-//                'ess_type' =>$users_ess_type,
-//                'ess_id' => $users_ess_id,
-//                'active' => 1,
-//                'created_at' => date("Y-m-d"),
-//                'updated_at' => date("Y-m-d")
-//            );
-            $login_data = array(
-                'user_type_id'       => 2,
-                'ess_type'        =>   $users_ess_type,
-                'ess_id'        =>   $users_ess_id,
-                'name'        =>   $request->name,
-                'username'        =>  $request->username,
-                'password'        =>    bcrypt($request->password),
-                'email'        =>    $request->email,
-                'photo'        =>   "no_image.jpg",
-                'active'        =>  1,
-                'created_at' => date("Y-m-d"),
-                'updated_at' => date("Y-m-d")
+                $login_data = array(
+                    'user_type_id'       => 2,
+                    'ess_type'        =>   "Cash",
+                    'ess_id'        =>   0,
+                    'name'        =>   $request->name,
+                    'username'        =>  $request->username,
+                    'password'        =>    bcrypt($request->password),
+                    'email'        =>    $request->email,
+                    'photo'        =>   "no_image.jpg",
+                    'active'        =>  1,
+                    'created_at' => date("Y-m-d"),
+                    'updated_at' => date("Y-m-d")
 
-            );
+                );
 
-            $user = Membership::create($login_data);
+                $user = Membership::create($login_data);
 
-            $personal_data = array(
-                'ref_member_personal_info_membership_id' => $user->id,
-                'member_first_name' => $request->member_first_name,
-                'member_last_name' => $request->member_last_name,
-                'member_birth_date' => $request->member_birth_date,
-                'member_email_address' => $request->email,
-                'member_gender' => $request->member_gender,
-                'member_address' => $request->member_address,
-                'member_zip_code' => $request->member_zip_code,
-                'member_tax_reg_no' => $request->member_tax_reg_no,
-                'member_personal_info_creating_datetime' => date("Y-m-d"),
-                'member_personal_info_editing_datetime' => date("Y-m-d")
-            );
+                $personal_data = array(
+                    'ref_member_personal_info_membership_id' => $user->id,
+                    'member_first_name' => $request->member_first_name,
+                    'member_last_name' => $request->member_last_name,
+                    'member_birth_date' => $request->member_birth_date,
+                    'member_email_address' => $request->email,
+                    'member_gender' => $request->member_gender,
+                    'member_address' => $request->member_address,
+                    'member_zip_code' => $request->member_zip_code,
+                    'member_tax_reg_no' => $request->member_tax_reg_no,
+                    'member_personal_info_creating_datetime' => date("Y-m-d"),
+                    'member_personal_info_editing_datetime' => date("Y-m-d")
+                );
 
-            $job_data = array(
-                'ref_member_job_info_membership_id' => $user->id,
-                'member_command_code' => $request->member_command_code,
-                'member_command_name' => $request->member_command_name,
-                'member_rank' => $request->member_rank,
-                'member_shield' => $request->member_shield,
-                'member_appointment_date' => $request->member_appointment_date,
-                'member_promoted_date' => $request->member_promoted_date,
-                'member_boro' => $request->member_boro,
-                'member_benificiary' => $request->member_benificiary,
-                'member_reference_no' => $request->member_reference_no,
-                'member_retired' => $request->member_retired,
-                'member_job_info_creating_datetime' => date("Y-m-d"),
-                'member_job_info_editing_datetime' => date("Y-m-d")
-            );
+                $job_data = array(
+                    'ref_member_job_info_membership_id' => $user->id,
+                    'member_command_code' => $request->member_command_code,
+                    'member_command_name' => $request->member_command_name,
+                    'member_rank' => $request->member_rank,
+                    'member_shield' => $request->member_shield,
+                    'member_appointment_date' => $request->member_appointment_date,
+                    'member_promoted_date' => $request->member_promoted_date,
+                    'member_boro' => $request->member_boro,
+                    'member_benificiary' => $request->member_benificiary,
+                    'member_reference_no' => $request->member_reference_no,
+                    'member_retired' => $request->member_retired,
+                    'member_job_info_creating_datetime' => date("Y-m-d"),
+                    'member_job_info_editing_datetime' => date("Y-m-d")
+                );
 
-            $payment_data = array(
-                'ref_membership_id' => $user->id,
-                'membership_payment_ess' => $request->membership_payment_ess,
-                'membership_payment_amount' => 0,
-                'membership_payment_datetime' => date("Y-m-d"),
-                'membership_payment_creating_datetime' => date("Y-m-d"),
-                'membership_payment_editing_datetime' => date("Y-m-d")
-            );
+                $payment_data = array(
+                    'ref_membership_id' => $user->id,
+                    'membership_payment_ess' => $request->membership_payment_ess,
+                    'membership_payment_amount' => 0,
+                    'membership_payment_datetime' => date("Y-m-d"),
+                    'membership_payment_creating_datetime' => date("Y-m-d"),
+                    'membership_payment_editing_datetime' => date("Y-m-d")
+                );
 
 
-            MemberPersonalInfo::create($personal_data);
-            MemberJobInfo::create($job_data);
-            MembershipPayment::create($payment_data);
+                MemberPersonalInfo::create($personal_data);
+                MemberJobInfo::create($job_data);
+                MembershipPayment::create($payment_data);
 
-            DB::commit();
-          //  $this->membershipPayment($user->id);
+                DB::commit();
+                //  $this->membershipPayment($user->id);
 
-            $success['token'] =  $user->createToken('MyApp')-> accessToken;
-            $success['name'] =  $user->name;
-		    return response()->json(['success'=>true,'data'=>$success], $this-> successStatus);
+                $success['token'] =  $user->createToken('MyApp')-> accessToken;
+                $success['name'] =  $user->name;
+                return response()->json(['success'=>true,'data'=>$success], $this-> successStatus);
 
-        } catch (\Exception $e) {
-            DB::rollback();
-             return response()->json(['success'=>false,'error'=>$e], 401);
+            } catch (\Exception $e) {
+                DB::rollback();
+                return response()->json(['success'=>false,'error'=>$e], 401);
+            }
         }
+        else
+        {
+            DB::beginTransaction();
+
+            try {
+
+
+                $ess_data = array(
+                    'type'       =>  "Cash",
+                    'name'        =>   $request->name,
+                    'email'        =>   $request->email,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'active'        =>   1,
+
+                );
+
+                $create_ess_member=EssMember::create($ess_data);
+
+                $login_data = array(
+                    'user_type_id'       => 2,
+                    'ess_type'        =>   "Cash",
+                    'ess_id'        =>   isset($create_ess_member->id)?$create_ess_member->id:555,
+                    'name'        =>   $request->name,
+                    'username'        =>  $request->username,
+                    'password'        =>    bcrypt($request->password),
+                    'email'        =>    $request->email,
+                    'photo'        =>   "no_image.jpg",
+                    'active'        =>  1,
+                    'created_at' => date("Y-m-d"),
+                    'updated_at' => date("Y-m-d")
+
+                );
+
+                $user = Membership::create($login_data);
+
+                $personal_data = array(
+                    'ref_member_personal_info_membership_id' => $user->id,
+                    'member_first_name' => $request->member_first_name,
+                    'member_last_name' => $request->member_last_name,
+                    'member_birth_date' => $request->member_birth_date,
+                    'member_email_address' => $request->email,
+                    'member_gender' => $request->member_gender,
+                    'member_address' => $request->member_address,
+                    'member_zip_code' => $request->member_zip_code,
+                    'member_tax_reg_no' => $request->member_tax_reg_no,
+                    'member_personal_info_creating_datetime' => date("Y-m-d"),
+                    'member_personal_info_editing_datetime' => date("Y-m-d")
+                );
+
+                $job_data = array(
+                    'ref_member_job_info_membership_id' => $user->id,
+                    'member_command_code' => $request->member_command_code,
+                    'member_command_name' => $request->member_command_name,
+                    'member_rank' => $request->member_rank,
+                    'member_shield' => $request->member_shield,
+                    'member_appointment_date' => $request->member_appointment_date,
+                    'member_promoted_date' => $request->member_promoted_date,
+                    'member_boro' => $request->member_boro,
+                    'member_benificiary' => $request->member_benificiary,
+                    'member_reference_no' => $request->member_reference_no,
+                    'member_retired' => $request->member_retired,
+                    'member_job_info_creating_datetime' => date("Y-m-d"),
+                    'member_job_info_editing_datetime' => date("Y-m-d")
+                );
+
+                $payment_data = array(
+                    'ref_membership_id' => $user->id,
+                    'membership_payment_ess' => $request->membership_payment_ess,
+                    'membership_payment_amount' => 0,
+                    'membership_payment_datetime' => date("Y-m-d"),
+                    'membership_payment_creating_datetime' => date("Y-m-d"),
+                    'membership_payment_editing_datetime' => date("Y-m-d")
+                );
+
+
+                MemberPersonalInfo::create($personal_data);
+                MemberJobInfo::create($job_data);
+                MembershipPayment::create($payment_data);
+
+                DB::commit();
+                //  $this->membershipPayment($user->id);
+
+                $success['token'] =  $user->createToken('MyApp')-> accessToken;
+                $success['name'] =  $user->name;
+                return response()->json(['success'=>true,'data'=>$success], $this-> successStatus);
+
+            } catch (\Exception $e) {
+                DB::rollback();
+                return response()->json(['success'=>false,'error'=>$e], 401);
+            }
+        }
+
+
+
 
     }
 
@@ -217,11 +285,11 @@ class PassportController extends Controller
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
 
-            if (!$user->active)
+            if ($user->active==0)
                 return response()->json(['success' => false, 'error' => 'You are deactive by admin for some reason. Please contact with site admin']);
 
-            if ($user->ess_type !== "Online Payment" && $user->ess_type !== "Paid")
-                return response()->json(['success' => false, 'error' => 'Your subscription have to be renew. Please contact with site admin']);
+//            if ($user->ess_type !== "Online Payment" || $user->ess_type !== "Paid")
+//                return response()->json(['success' => false, 'error' => 'Your are not paid user or your subscription have to be renew. Please contact with site admin']);
 
 
             $member_devices = MemberDevice::create([
